0e7f078a897e72c8bc62ae65cc3b9fe987731d22
Tomoki Nakayama <t_nakayama@yumemi.co.jp>
2024-06-18 22:17:16 +0900

    fix:  初期画面から複数のゲームを選択出来る様実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
-import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
-import 'package:space_shoooter/ViewModel/space_shooter_game.dart';
+import 'package:space_shoooter/View/space_shooter.dart';
 
-void main() {
-  runApp(GameWidget(game: SpaceShooterGame()));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Game with Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('''Let's play Game!!!'''),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Spacer(),
+              _NavigationButton(),
+              _NavigationButton(),
+              _NavigationButton(),
+              Spacer(flex: 2),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavigationButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SpaceShooter()),
+          );
+        },
+        child: Text('Space Shooter 🔥🚀🚀🚀'));
+  }
 }
